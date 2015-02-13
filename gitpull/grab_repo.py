0760916fc448e0de0b4d4b45ddd24b2164e4a731
thoppe <travis.hoppe@gmail.com>
2015-02-13 18:58:41 -0500

    Downloader just downloads, will tokenize in another file.

--- gitpull/grab_repo.py
+++ gitpull/grab_repo.py
@@ -1,10 +1,14 @@
-import glob, json, sqlite3, os, json, string, hashlib
+import glob, json, sqlite3, os, json, string, hashlib, time
 from API_github import *
 import tempfile, collections
+import glob2 # Recursive globber
 
-# Recursive globber
-import glob2
+# Storage for repos
+os.system("mkdir -p repos")
 
+cmd_grab = "curl -L https://api.github.com/repos/{full_name}/tarball > repos/{full_name}.tar.gz"
+
+# Connect to the DB that has been through parse_public.py
 f_repo_info = "db/repo_info.db"
 conn = sqlite3.connect(f_repo_info)
 
@@ -13,27 +17,33 @@ cursor = conn.execute('SELECT * FROM repo_info LIMIT 1')
 cols = list(map(lambda x: x[0], cursor.description))
 
 cmd_select = '''
-SELECT full_name, default_branch FROM repo_info
-WHERE created_at IS NOT NULL AND size>0 LIMIT 1'''
+SELECT full_name FROM repo_info
+WHERE created_at IS NOT NULL AND size>0'''
 
-git_url = "git://github.com/{}.git"
-cmd_git = "git clone {url} --branch {branch} --single-branch {folder}"
+#git_url = "git://github.com/{}.git"
+#cmd_git = "git clone {url} --branch {branch} --single-branch {folder}"
 
 # Load the extensions
 with open("filetypes/extensions.json") as FIN:
     extensions = json.load(FIN)
 
-def clone_repo(full_name, branch,folder):
+def download_repo_tar(full_name):
 
-    payload = {
-        "url":git_url.format(full_name),
-        "branch":branch,
-        "folder":folder
-    }
+    user_dir = full_name.split('/')[0]
+    os.system("mkdir -p repos/{}".format(user_dir))
 
-    cmd = cmd_git.format(**payload)
-    os.system(cmd)
+    f_out = "repos/{}.tar.gz".format(full_name)
 
+    if not os.path.exists(f_out):
+        check_limits()
+        print "DOWNLOADING: ", f_out
+        cmd = cmd_grab.format(full_name=full_name)
+        os.system(cmd)
+        
+        # Be nice
+        time.sleep(1.0) 
+
+'''
 def iter_repo(folder):
     all_files = glob2.glob("{}/**/*".format(folder))
     all_files_ext = ['.'+f.split('.')[-1][:10] for f in all_files]
@@ -61,18 +71,16 @@ def tokenize(f_code):
     tokens = collections.Counter(adjusted) 
 
     return md5,tokens
-  
+'''     
 
-    
+for (full_name,) in conn.execute(cmd_select):
+    download_repo_tar(full_name)
 
-for [full_name,branch] in conn.execute(cmd_select):
+    # TOKENIZE IN A DIFFERENT PROGRAM
 
-    folder = "tmp_git"
-    #clone_repo(full_name, branch,folder)
-
-    for language, f_code in iter_repo(folder):
-        md5, tokens = tokenize(f_code)
-        print md5, tokens
+    #for language, f_code in iter_repo(folder):
+    #    md5, tokens = tokenize(f_code)
+    #    print md5, tokens
 
 
         
