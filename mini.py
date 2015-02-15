f5cd0d2094a1f78c5b2efbb9413ab6a1443ca8be
thoppe <travis.hoppe@gmail.com>
2015-02-14 23:22:46 -0500

    Cleaned mini code

--- mini.py
+++ mini.py
@@ -1,23 +1,24 @@
 import tokenize, io
-import pyminifier.minification as mini
-
+import pyminifier.minification, pyminifier.token_utils
 
 def listified_tokenizer(source):
     """Tokenizes *source* and returns the tokens as a list of lists."""
     io_obj = io.StringIO(source)
     return [list(a) for a in tokenize.generate_tokens(io_obj.readline)]
 
-class placeholder:
-    pass
+# Dummy options to allow a call to pyminifier.minification.minify
+class __placeholder: pass
+__mini_options = __placeholder()
+__mini_options.tabs = False
 
-options = placeholder()
-options.tabs = False
+def clean_pycode(raw):
+    tokens = pyminifier.token_utils.listified_tokenizer(raw)
+    return pyminifier.minification.minify(tokens,__mini_options)
 
 with open("tumult.py") as FIN:
     raw = unicode(FIN.read())
+    small = clean_pycode(raw)
+    print len(raw), len(small)
 
-tokens = listified_tokenizer(raw)
-print len(raw)
-small = mini.minify(tokens,options)
-print len(small)
+
 
