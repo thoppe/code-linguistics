cc998a020432d4ef8a5e646e73427d156dd4b7fe
thoppe <travis.hoppe@gmail.com>
2015-02-23 23:49:49 -0500

    Fitting function using maximum likelihood now working!

--- fit_tokens/fit.py
+++ fit_tokens/fit.py
@@ -1,68 +1,81 @@
-# INPROGRESS
-
 import numpy as np
 import sqlite3
-from scipy.optimize import fmin
+from scipy.optimize import fmin, minimize
 import mpmath
 mpmath.dps = 15
 
-language = "javascript"
+language = "java"
+#language = "python"
 f_conn = "db_tokens/{}.db".format(language)
 conn = sqlite3.connect(f_conn)
 
 cmd_grab = '''SELECT count FROM tokens'''
-COUNTS = np.array([x for (x,) in conn.execute(cmd_grab)])
+COUNTS = np.array([x for (x,) in conn.execute(cmd_grab)],dtype=float)
 FREQ = COUNTS
 FREQ = np.sort(FREQ)[::-1]
 RANK = np.arange(1, len(FREQ)+1)
 
-def F(r, params):
-    alpha,beta = params
-    return (r+beta)**(-alpha)
+total_FREQ = FREQ.sum()
+FREQ /= total_FREQ
+
+class rank_fit_function(object):
+
+    def __init__(self,FREQ,RANK):
+        self.FREQ = FREQ
+        self.RANK = RANK
+        self.px = None
+
+    def normalization_constant(self,params):
+        f = lambda r:self.F(r,params)
+        return 1.0/mpmath.sumem(f, [1, mpmath.inf])
+
+    def err_func(self,p):
+        C = self.normalization_constant(p)
+        val  = mpmath.log(C)
+        val += (self.FREQ*self.ln_F(self.RANK,p)).sum()
+        val *= -1
+        print p, val
+        return val
+
+    def optimize(self):
+
+        self.result = minimize(self.err_func, 
+                               self.p0,
+                               method="Nelder-Mead")
+        self.px = self.result.x
+        return self.px
+
+    def fitted_ranked(self):
+        assert(self.px is not None)
+
+        Y_FIT = self.F(self.RANK, self.px)
+        Y_FIT /= Y_FIT.sum()
+        return Y_FIT
+        
+
+def fit_factory(FREQ, RANK, F=None, ln_F=None, p0=None):
+    fit = rank_fit_function(FREQ,RANK)
+    fit.F = F
+    fit.ln_F = ln_F
+    fit.p0 = p0
+    return fit
 
 
-def normalization_constant(F, params):
-    f = lambda r:F(r,params)
-    return 1.0/mpmath.sumem(f, [1, mpmath.inf])
+power_law = {"F"   : lambda r,p:r**(-p[0]),
+             "ln_F": lambda r,p:-p[0]*np.log(r),
+             "p0"  : (1.5,)}
 
-def err_func(p,X):
-    C = normalization_constant(F,p)
-    
-    print C
-    print X
-    exit()
-    return 1.0
+shifted_power_law = {"F"   : lambda r,p:(r+p[1])**(-p[0]),
+                     "ln_F": lambda r,p:-p[0]*np.log(r+p[1]),
+                     "p0"  : (1.5,2.0)}
 
-p0 = (alpha,beta) = 3.0,0.0
-C = normalization_constant(F,p0)
-
-fmin(err_func, p0,args=(FREQ,))
-exit()
+#fit = fit_factory(FREQ,RANK,**power_law)
+fit = fit_factory(FREQ,RANK,**shifted_power_law)
+fit.optimize()
 
 import pylab as plt
-plt.loglog(X,C*F(X,p0),lw=3,alpha=.50)
-plt.loglog(X,C*F(X,p0),'ro',alpha=.50)
 
-
+Y_FIT = fit.fitted_ranked()
+plt.loglog(RANK,Y_FIT,lw=3,alpha=.50)
+plt.loglog(RANK,FREQ,'ro',mew=0,alpha=.50)
 plt.show()
-
-print C
-exit()
-
-
-print fmin(min_func, (alpha,beta))
-exit()
-
-
-C = normalization_constant(RANK,F,(alpha,beta))
-print C
-
-FIT = F(RANK, (alpha,beta))
-
-import pylab as plt
-plt.loglog(RANK, FREQ,'o',lw=0,alpha=.75)
-plt.loglog(RANK, FIT ,'r--',alpha=.75)
-plt.loglog(RANK, C*FIT ,'b--',alpha=.75)
-plt.loglog(RANK, FIT/C ,'g--',alpha=.75)
-plt.show()
-
